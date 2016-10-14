cd2ef0ef227b9c1793cea7eadf74d949b1222be8
Yang Wenqiang <1106911190@qq.com>
2016-10-14 21:01:30 +0800

    update aoban

--- Exercise/laoban.c
+++ Exercise/laoban.c
@@ -342,3 +342,124 @@ int main(int argc, char const *argv[])
 }
 
 **************************************************************************************/
+
+/*********************************** 第八题 **********************************************
+#include "stdio.h"
+#include "conio.h"
+#include "stdlib.h"
+
+typedef char datatype;
+
+struct Node {
+	datatype 		element;
+	struct 	Node 	*next;
+};
+struct Node *top;
+
+struct Node * CreatListR1(){
+	char 			ch;
+	struct Node		*head,*s,*r;
+	head = (struct Node*)malloc(sizeof(struct Node));
+	r = head;
+	printf("Please Input String Which Ends with '@' :");	
+	while((ch=getche())!='@'){
+		s = (struct Node*)malloc(sizeof(struct Node));
+		s->element = ch;
+		r->next = s;
+		r = s;
+	}
+	r->next = NULL;
+	return head;
+}
+
+void Output(struct Node *L){
+	struct Node 	*head;
+	head = L;
+	head = head->next;
+	while(head->next!=NULL){
+		printf("%c",head->element );
+		head = head->next;
+	}
+	printf("%c",head->element );
+}
+
+datatype *PopL(struct Node *S){
+	datatype *ret;
+	if(S==NULL){
+		printf("Stack is underflow\n");
+		return NULL;
+	}else{
+		ret = (datatype *)malloc(sizeof(datatype));
+		*ret = S->element;
+		return ret;
+	}
+}
+
+struct Node * PushL(struct Node *S,datatype e){
+	struct Node *p;
+	p=(struct Node *)malloc(sizeof(struct Node));
+	p->element = e;
+	p->next    = S;
+	S = p;
+	return S;
+}
+
+int JudgeL(struct Node *L){
+	int 				length=0;
+	struct Node 		*p;
+	struct Node 		*head;
+	head = L;
+	while(head!=NULL){
+		length++;
+		head = head->next;
+	}
+	length--;
+	L = L->next;
+	for (int i = 0; i < (length+1)/2; i++)
+	{
+		p = PushL(p,L->element);
+		L = L->next;
+	}
+	if(length%2==0){
+		while(L!=NULL){
+			if(*PopL(p)==L->element){
+				p = p->next;
+				L = L->next;
+			}else{
+				printf("%c\n",*PopL(p) );
+				return 0;
+			}			
+		}
+		return 1;
+	}else{
+		p = p->next;
+		while(L!=NULL){
+			if(*PopL(p)==L->element){
+				p = p->next;
+				L = L->next;
+			}else{
+				return 0;
+			}			
+		}
+		return 1;
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	struct Node 	*L;
+	int flag;
+	L = CreatListR1();
+	printf("\n");
+	Output(L);		
+	printf("\n");	
+	flag=JudgeL(L);
+	if (flag){
+		printf("Yes \n");
+	}else {
+		printf("No \n");
+	}
+	return 0;
+}
+
+********************************************************************************************/
