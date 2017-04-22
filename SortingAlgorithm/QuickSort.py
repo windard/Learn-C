3ba0212a65c7c3e44cb67a90ab6f4d747c2077a6
Yang Wenqiang <1106911190@qq.com>
2017-04-22 23:56:22 +0800

    update quickstort

--- SortingAlgorithm/QuickSort.py
+++ SortingAlgorithm/QuickSort.py
@@ -3,11 +3,13 @@
 
 def quickSort(a):
 
-    return a if len(a) <=1 else quickSort([x for x in a[1:] if x < a[0]]) + [a[0]] + quickSort([x for x in a[1:] if x >= a[0]])
+    return a if len(a) <= 1 else quickSort([x for x in a[1:] if x < a[0]]) + [a[0]] + quickSort([x for x in a[1:] if x >= a[0]])
 
 
 if __name__ == '__main__':
     a = [10, 1, 23, -5, 0, 78, 11, 104, 65, -1, 12, 23, 36, 3, 53]
     print a
+
+    quickSort = lambda a: a if len(a) <= 1 else quickSort([x for x in a[1:] if x < a[0]]) + [a[0]] + quickSort([x for x in a[1:] if x >= a[0]])
 
     print quickSort(a)
